b05acaf341ca436e8892b54f6aa4ca7b78d5610b
Prateek Garg <prateekgarg1110@gmail.com>
2025-11-30 23:00:12 +0530

    Update Day78.c

--- Day78.c
+++ Day78.c
@@ -1,7 +1,7 @@
 //Q: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
 
-#include <stdio.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<ctype.h>
 
 int main() {
     FILE *fp;
@@ -42,5 +42,6 @@ File: text.txt (Content: Welcome to C language)
 Output 1:
 Vowels: 8
 Consonants: 10
+
 
 */
